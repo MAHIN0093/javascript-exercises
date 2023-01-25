657f4aa3a08192da9543a5f1351663c03aed3867
Mashrafi Mahin <101042038+MAHIN0093@users.noreply.github.com>
2023-01-25 19:01:05 +0600

    1 and 2 passed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(key,iterations) {
+    if (iterations < 0){
+        return "ERROR";
+    }
+    let sudo = ''
+for (let i = 0; i < iterations; i++) {
+    sudo += key;   
+}
+return sudo;
 };
 
 // Do not edit below this line
